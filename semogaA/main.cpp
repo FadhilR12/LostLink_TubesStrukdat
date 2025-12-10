ccba217200c77d22d1e842d76e9a54be2269e2cb
Fadhil Rahman Yuriana <fadhilrahman0814@gmail.com>
2025-12-10 14:54:32 +0700

    update main

--- semogaA/main.cpp
+++ semogaA/main.cpp
@@ -2,25 +2,52 @@
 #include "lostLink.h"
 using namespace std;
 
+adrNode root = nullptr;
+infotype x;
+string nama, lokasi;
+int id, kategori;
+
 void main_menu(){
-    cout << "=== LostLink ===" << endl;
-    cout << "1. Add New Item" << endl;
-    cout << "2. View All Items" << endl;
-    cout << "3. Search Item" << endl;
+    cout << "=========== LostLink ===========" << endl;
+    cout << "1. Tambahkan Item" << endl;
+    cout << "2. Lihat Semua Item" << endl;
+    cout << "3. Cari Item" << endl;
     cout << "4. Update Item" << endl;
-    cout << "5. Delete Item" << endl;
-    cout << "6. View Items by Category" << endl;
-    cout << "7. Exit" << endl;
+    cout << "5. Hapus Item" << endl;
+    cout << "6. Lihat Items Berdasarkan Kategori" << endl;
+    cout << "7. Keluar" << endl;
     cout << "Select an option (1-7): ";
 }
+
+infotype menuTambahItem(){
+    infotype x;
+    cout << "=========== Tambah Item Baru ===========" << endl;
+    cout << "Masukkan ID Item: ";
+    cin >> x.id;
+    cout << "Masukkan Nama Item: ";
+    cin >> x.nama;
+    cout << "Masukkan Lokasi Item: ";
+    cin >> x.lokasi;
+    if (id <= 100) {
+        x.kategori = 1; // Elektronik
+    } else if (id <= 200) {
+        x.kategori = 2; // Pakaian
+    } else if (id <= 300) {
+        x.kategori = 3; // Makanan
+    } else if (id <= 400) {
+        x.kategori = 4; // Buku
+    } else {
+        x.kategori = 5; // Lainnya
+    }
+    cout << "Item berhasil ditambahkan!" << endl;
+    return x;
+}
+
 
 int main()
 {
     int input = 0;
-    adrNode root = nullptr;
-    infotype x;
-    string name;
-
+    
     createTree(root);
 
     while (input != 7){
@@ -28,10 +55,14 @@ int main()
         cin >> input;
         switch (input){
             case 1:
-                // Code to add new item
+                x = menuTambahItem();
+                insertNode(root, createNode(x));
+                cout << "Kembali ke menu utama..." << endl;
+                cout << endl;
                 break;
             case 2:
-                // Code to view all items
+                cout << "=========== Semua Item ===========" << endl;
+                inorder(root);
                 break;
             case 3:
                 // Code to search item by ID
